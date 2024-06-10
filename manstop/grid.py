dbe91b376caaefd4f3829b8a1f93abb54251ba82
Trevor Murphy <trevor.m.murphy@gmail.com>
2024-06-09 21:22:17 -0700

    Wire up a simple grid.
    Along the way I'm refactoring the class model.  Seems fine.

---
+++ manstop/grid.py
@@ -0,0 +1,25 @@
+from manstop.model import Node, Street, FixedSignalFactory
+
+def fromFile(f):
+    "Something that can parse a file would be nice"
+
+def fromCoordinates(x, y):
+    "Wire up a default grid sized X streets by Y streets"
+    default_walktime = 100
+    default_lighttime = 130
+
+    traffic_signal_factory = FixedSignalFactory(default_lighttime, 0)
+
+    all_nodes = [[Node(traffic_signal_factory) for _ in range(y)] for _ in range(x)]
+    for i in range(x):
+        for j in range(y):
+            if i + 1 < x:
+                next_node_x = all_nodes[i + 1][j]
+                next_street_x = Street("E", default_walktime, next_node_x)
+                all_nodes[i][j].addStreet(next_street_x)
+            if j + 1 < y:
+                next_node_y = all_nodes[i][j + 1]
+                next_street_y = Street("N", default_walktime, next_node_y)
+                all_nodes[i][j].addStreet(next_street_y)
+
+    return all_nodes[0][0]
--- manstop/model.py
+++ manstop/model.py
@@ -1,12 +1,22 @@
+from abc import ABC, abstractmethod
+from random import randrange
+
 class Node:
     "An intersection in the grid"
 
-    def __init__(self, streets):
-        self.streets = streets
+    def __init__(self, trafficSignalFactory):
+        self.trafficSignalFactory = trafficSignalFactory
+        self.streets = []
+
+    def addStreet(self, street):
+        self.streets.append(street)
+
+    def addStreets(self, streets):
+        self.streets.extend(streets)
 
     def accept(self, walker):
-        walker.setChoices(self.streets)
-
+        walker.setStreets(self.streets)
+        walker.setTrafficSignalFactory(self.trafficSignalFactory)
 
 class Walker:
     "A person walking the grid"
@@ -14,29 +24,39 @@ class Walker:
     def __init__(self, strategy, startNode):
         self.now = 0          # should use stdlib
         self.strategy = strategy
-        self.choices = []
+        self.streets = []
+        self.trafficSignalFactory = None
         startNode.accept(self)
 
-    def walkTo(self, nextNode, walkTime):
-        self.now += walkTime
-        nextNode.accept(self)
+    def setStreets(self, streets):
+        self.streets = streets
 
-    def setChoices(self, choices):
-        self.choices = choices
+    def setTrafficSignalFactory(self, trafficSignalFactory):
+        self.trafficSignalFactory = trafficSignalFactory
+
+    def cross(self, street, trafficSignal):
+        "How much time it takes the WALKER to cross"
+        self.now += street.getWalkTime()
+        self.now += trafficSignal.getDelay(street.getOrientation())
+
+    def waitFor(self, waitTime):
+        self.now += waitTime
 
     def walk(self):
-        if self.choices:
-            street = self.strategy.choose(self.choices, self.now)
-            street.acceptAt(self, self.now)
+        if self.streets:
+            trafficSignal = self.trafficSignalFactory.createAt(self.now)
+            street = self.strategy.choose(self.streets, trafficSignal)
+            self.cross(street, trafficSignal)
+            street.accept(self)
 
 class Strategy:
     "An algo for a Walker to use at a Node"
 
     @staticmethod
-    def choose(choices, t):
+    def choose(streets, trafficSignal):
         "Default implementation is just take a green when you can"
-        for street in choices:
-            signal = street.getTrafficSignal.at(t)
+        for street in streets:
+            signal = trafficSignal.getStatus(street.getOrientation())
             if signal == "green":
                 return street
             else:
@@ -47,32 +67,73 @@ class Strategy:
 class Street:
     "A path to a Node"
 
-    def __init__(self, trafficSignal, nextNode):
-        self.walkTime = 100
-        self.trafficSignal = trafficSignal
+    def __init__(self, orientation, walkTime, nextNode):
+        self.orientation = orientation
+        self.walkTime = walkTime
         self.nextNode = nextNode
 
-    def getTrafficSignal(self):
-        return self.trafficSignal
+    def getWalkTime(self):
+        return self.walkTime
 
-    def acceptAt(self, walker, time):
-        totalTime = self.walkTime + self.trafficSignal.timeRemainingAt(time)
-        walker.walkTo(self.nextNode, totalTime)
+    def getOrientation(self):
+        "NESW"
+        return self.orientation
+
+    def accept(self, walker):
+        self.nextNode.accept(walker)
 
 
 class TrafficSignal:
-    "Changes colors over time"
+    """Changes colors over time.
 
-    def __init__(self, period, offset):
-        self.period = period
+    halfPeriod: amount of time between light changes
+    offset: where in the cycle the light begins (0 == begin at the start of the red light)
+    checkTime: the current time we're checking the signal
+"""
+
+    def __init__(self, halfPeriod, offset, checkTime):
+        # Save the time left to the next green, or 0 if already green
+        normalized = (checkTime + offset) % (halfPeriod * 2)
+        if normalized < halfPeriod:
+            self.status = {"N": "red", "E": "green"}
+            self.delay = {"N": halfPeriod - normalized, "E": 0}
+        else:
+            self.status = {"N": "green", "E": "red"}
+            self.delay = {"N": 0, "E": 2 * halfPeriod - normalized}
+
+    def getStatus(self, orientation):
+        "What color is it now, looking towards ORIENTATION?"
+        return self.status[orientation]
+
+    def getDelay(self, orientation):
+        "How long to hold up WALKER before they head towards ORIENTATION"
+        return self.delay[orientation]
+
+
+class TrafficSignalFactory(ABC):
+    "Control how the traffic signals interact"
+
+    @abstractmethod
+    def createAt(self, t):
+        pass
+
+
+class FixedSignalFactory(TrafficSignalFactory):
+    "Return traffic lights that share a common period and offset"
+
+    def __init__(self, halfPeriod, offset):
+        self.halfPeriod = halfPeriod
         self.offset = offset
-        self.redTime = period // 2
 
-    def at(self, t):
-        "What color is it now?"
-        return "green" if self.timeRemainingAt(t) == 0 else "red"
+    def createAt(self, t):
+        return TrafficSignal(self.halfPeriod, self.offset, t)
 
-    def timeRemainingAt(self, t):
-        "Time left to the next green, or 0 if currently green"
-        normalized = (t + self.offset) % self.period
-        return min(self.redTime - normalized, 0)
+
+class RandomSignalFactory(TrafficSignalFactory):
+    "Return traffic lights with a common period but random offset"
+
+    def __init__(self, halfPeriod):
+        self.halfPeriod = halfPeriod
+
+    def createAt(self, t):
+        return TrafficSignal(self.halfPeriod, randrange(2 * self.halfPeriod), t)
